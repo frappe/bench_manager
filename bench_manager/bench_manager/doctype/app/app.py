c0050b169611b7857b8fc5953595b9a0dbf38f85
Ameya Shenoy <shenoy.ameya@gmail.com>
2017-08-07 16:44:32 +0530

    site creation, site deletion, site_config editing and app creation added

--- bench_manager/bench_manager/doctype/app/app.py
+++ bench_manager/bench_manager/doctype/app/app.py
@@ -6,45 +6,45 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from subprocess import check_output, Popen, PIPE
+import sys
 
 class App(Document):
-	pass
-	# def validate(self):
-	# 	if self.get("__islocal"):
-	# 		self.create_app()
-	# 	else:
-	# 		seld.update_app_details()
+	def validate(self):
+		if self.get("__islocal"):
+			self.create_app()
+		else:
+			self.update_app_details()
 
-	# def create_app(self):
-	# 	app_list = check_output("cd ../apps && ls",
-	# 		shell=True).split("\n")
-	# 	if self.app_name in app_list:
-	# 		frappe.throw("App: "+ self.app_name + " already exists, but most\
-	# 			likely there isn't a log of this app. Please click sync to\
-	# 			refresh your app list!")
-	# 	else:
-	# 		terminal = Popen("cd .. && bench new-app " + self.app_name,
-	# 			stdin=PIPE, shell=True)
-	# 		self.app_title = self.app_title if self.app_title!='' else '\n'
-	# 		self.app_description = self.app_description + '\n'
-	# 		self.app_publisher = self.app_publisher + '\n' 
-	# 		self.app_email = self.app_email + '\n'
-	# 		self.app_icon = self.app_icon if self.app_icon!='' else '\n'
-	# 		self.app_color = self.app_color if self.app_color!='' else '\n'
-	# 		terminal.communicate(self.app_title + self.app_description + 
-	# 			self.app_publisher + self.app_email + self.app_icon + 
-	# 			self.app_color)
+	def create_app(self):
+		app_list = check_output("cd ../apps && ls",
+			shell=True).split("\n")
+		if self.app_name in app_list:
+			frappe.throw("App: "+ self.app_name + " already exists, but most\
+				likely there isn't a log of this app. Please click sync to\
+				refresh your app list!")
+		else:
+			if ' ' in self.app_name:
+				frappe.throw("The app name should be developer friendly, and \
+					should not contain spaces !")
+			else:
+				terminal = Popen("cd .. && bench new-app " + self.app_name,
+					stdin=PIPE, shell=True)
+				self.app_title = self.app_title + '\n'
+				self.app_description = self.app_description + '\n'
+				self.app_publisher = self.app_publisher + '\n' 
+				self.app_email = self.app_email + '\n'
+				self.app_icon = self.app_icon + '\n' if self.app_icon!=None else '\n'
+				self.app_color = self.app_color + '\n' if self.app_color!=None else '\n'
+				self.app_license = self.app_license + '\n' if self.app_license!=None else '\n'
+				terminal.communicate(self.app_title + self.app_description + 
+					self.app_publisher + self.app_email + self.app_icon + 
+					self.app_color + self.app_license)
+				if self.app_title == None:
+					self.app_title = self.app_title.replace('-', ' ')
+					self.app_title = self.app_title.replace('_', ' ')
 
-	# def on_trash(self):
-	# 	app_list = check_output("cd ../apps && ls",
-	# 		shell=True).split("\n")
-
-	# 	if self.app_name in app_list:
-	# 		check_output("cd .. && bench remove-from-installed-apps "
-	# 			+self.app_name, shell=True)
-	# 	else:
-	# 		frappe.throw("App: "+ self.app_name+ " doesn't exist! Please click\
-	# 			sync to  refresh your app list!")
+	def on_trash(self):
+		frappe.throw('Not allowed!')
 
 	def update_app_details(self):
 		pass
--- bench_manager/bench_manager/doctype/site/site.py
+++ bench_manager/bench_manager/doctype/site/site.py
@@ -5,45 +5,98 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-import subprocess
 import json
+from subprocess import check_output, Popen, PIPE
 import re
+import os
 
 class Site(Document):
+	site_config_fields = ["auto_update", "background_workers", "developer_mode",
+		"file_watcher_port", "frappe_user", "gunicorn_workers",
+		"rebase_on_pull", "redis_cache", "redis_queue", "redis_socketio", 
+		"restart_supervisor_on_update", "serve_default_site", "shallow_clone",
+		"socketio_port", "update_bench_on_update", "webserver_port",
+		"root_password", "admin_password", "db_name", "db_password"]
+
+	def get_attr(self, varname):
+		return getattr(self, varname)
+
+	def set_attr(self, varname, varval):
+		return setattr(self, varname, varval)
+
 	def validate(self):
 		if self.get("__islocal"):
 			self.create_site()		
+			self.sync_site_config()
 		else:
+			self.update_app_list()
 			self.update_site_config()
 			self.sync_site_config()
 
 	def create_site(self):
-		site_list = subprocess.check_output("ls", shell=True).split("\n")
+		site_list = check_output("ls", shell=True).split("\n")
 		if self.site_name in site_list:
 			frappe.throw("Site: "+ self.site_name+ " already exists, but most\
 				likely there isn't a log of this site. Please click sync to\
 				refresh your site list!")
 		else:
-			subprocess.check_output("cd .. && bench new-site "+self.site_name,
+			check_output("cd .. && bench new-site "+self.site_name,
 				shell=True)
 
 	def on_trash(self):
-		site_list = subprocess.check_output("ls", shell=True).split("\n")
+		site_list = check_output("ls", shell=True).split("\n")
 		if self.site_name in site_list:
-			subprocess.check_output("cd .. && bench drop-site "+self.site_name,
+			check_output("cd .. && bench drop-site "+self.site_name,
 				shell=True)
 		else:
 			frappe.throw("Site: "+ self.site_name+ " doesn't exists! Please\
 				click sync to refresh your site list!")
+		return
+
+	def update_app_list(self):
+		self.app_list = '\n'.join(self.get_installed_apps())
+
+	def get_installed_apps(self):
+		terminal = Popen("cd .. && bench --site "+self.site_name+" console", stdout=PIPE, stdin=PIPE, shell=True)
+		out, error = terminal.communicate("frappe.get_installed_apps()\nexit()")
+		out = out.split('\n')
+		return re.findall("u\'(.*?)\'", out[9])
 
 	def update_site_config(self):
-		self.app_list = '\n'.join(frappe.get_installed_apps()) #TODO: this is wrong, enter the console and retrive the code for the site in question
 		site_config_path = self.site_name+'/site_config.json'
 		common_site_config_path = 'common_site_config.json'
-		with open(site_config_path, 'r') as f:
-			site_config_data = json.load(f)
 		with open(common_site_config_path, 'r') as f:
 			common_site_config_data = json.load(f)
+		with open(site_config_path, 'r') as f:
+			site_config_data = json.load(f)
+
+		for site_config_field in self.site_config_fields:
+			site_config_data[site_config_field] = self.get_attr(site_config_field)
+
+		os.remove(site_config_path)
+		with open(site_config_path, 'w') as f:
+		    json.dump(site_config_data, f, indent=4)
 
 	def sync_site_config(self):
+		site_config_path = self.site_name+'/site_config.json'
+		common_site_config_path = 'common_site_config.json'
+		with open(common_site_config_path, 'r') as f:
+			common_site_config_data = json.load(f)
+			for site_config_field in self.site_config_fields:
+				try:
+					self.set_attr(site_config_field, common_site_config_data[site_config_field])
+				except:
+					pass
+		with open(site_config_path, 'r') as f:
+			site_config_data = json.load(f)
+			for site_config_field in self.site_config_fields:
+				try:
+					self.set_attr(site_config_field, site_config_data[site_config_field])
+				except:
+					pass
+
+	def add_app(self):
+		pass
+
+	def remove_app(self):
 		pass
