f983438632028d3db01c4f2349c35b1ad815aa35
Rushabh Mehta <rmehta@gmail.com>
2017-09-01 13:54:33 +0530

    [wip] cleanup site

--- bench_manager/bench_manager/utils.py
+++ bench_manager/bench_manager/utils.py
@@ -4,12 +4,16 @@
 
 from __future__ import unicode_literals
 import frappe
-from frappe.model.document import Document
-from subprocess import Popen, check_output, PIPE, STDOUT
+from subprocess import Popen, PIPE, STDOUT
 import shlex
 
 @frappe.whitelist()
 def console_command(doctype='', docname='', key='', bench_command='', app_name='', branch_name='', cwd='..'):
+	if site:
+		command = 'bench --site {site} {command}'.format(site=site, command=bench_command)
+	else:
+		command = 'bench {command}'.format(command=bench_command)
+
 	shell_commands = {
 		"install_app": ["bench --site "+ docname + " install-app " + app_name],
 		"remove_app": ["bench --site "+ docname + " uninstall-app " + app_name + " --yes"],
@@ -29,38 +33,47 @@ def console_command(doctype='', docname='', key='', bench_command='', app_name='
 		"new-site & get-app & install-erpnext": ["bench new-site "+docname,
 			"bench get-app erpnext https://github.com/frappe/erpnext.git","bench --site "+docname+" install-app erpnext"]
 	}
-	exec_str_list = shell_commands[bench_command]
+	commands = shell_commands[bench_command]
 	frappe.enqueue('bench_manager.bench_manager.utils.run_command',
-		exec_str_list=exec_str_list, cwd=cwd, doctype=doctype, key=key, docname=docname)
+		commands=commands, cwd=cwd, doctype=doctype, key=key, docname=docname)
 	return 0
 
 @frappe.whitelist()
-def run_command(exec_str_list, cwd, doctype, key, docname=' ', shell=False):
+def run_command(commands, cwd, doctype, key, docname=' ', shell=False):
 	start_time = frappe.utils.time.time()
 	console_dump = ''
-	doc = frappe.get_doc({'doctype': 'Bench Manager Command', 'key': key, 'source': doctype+': '+docname,
-		 'command': ' && '.join(exec_str_list), 'console': console_dump, 'status': 'Ongoing'})
+	doc = frappe.get_doc({
+		'doctype': 'Bench Manager Command',
+		'key': key,
+		'source': doctype+': '+docname,
+		'command': ' && '.join(commands),
+		'console': console_dump,
+		'status': 'Ongoing'
+	})
 	doc.insert()
 	frappe.db.commit()
 	try:
-		print exec_str_list
-		for str_to_exec in exec_str_list:
+		for command in commands:
 			if shell == False:
-				terminal = Popen(shlex.split(str_to_exec), shell=shell, stdin=PIPE, stdout=PIPE, stderr=STDOUT, cwd=cwd)
+				terminal = Popen(shlex.split(command), shell=shell, stdin=PIPE, stdout=PIPE, stderr=STDOUT, cwd=cwd)
 			else:
-				terminal = Popen(str_to_exec, shell=shell, stdin=PIPE, stdout=PIPE, stderr=STDOUT, cwd=cwd)
+				terminal = Popen(command, shell=shell, stdin=PIPE, stdout=PIPE, stderr=STDOUT, cwd=cwd)
 			for c in iter(lambda: terminal.stdout.read(1), ''):
 				frappe.publish_realtime(key, c, user=frappe.session.user)
 				console_dump += c
 		if terminal.wait():
-			_close_the_doc(start_time, key, console_dump, status='Failed', user=frappe.session.user)
+			_close_the_doc(start_time, key, console_dump, status='Failed',
+				user=frappe.session.user)
 		else:
-			_close_the_doc(start_time, key, console_dump, status='Success', user=frappe.session.user)
+			_close_the_doc(start_time, key, console_dump, status='Success',
+				user=frappe.session.user)
 	except:
-		_close_the_doc(start_time, key, console_dump, status='Failed', user=frappe.session.user)
+		_close_the_doc(start_time, key, console_dump, status='Failed',
+			user=frappe.session.user)
 	finally:
-		frappe.enqueue('bench_manager.bench_manager.doctype.bench_settings.bench_settings.sync_all')
-		frappe.publish_realtime("version-update")
+		if docname:
+			frappe.get_doc(doctype, docname).run_method('after_command', commands=commands)
+
 	return 0
 
 
