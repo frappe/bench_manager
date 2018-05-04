a8dd7f756194a0d742ad4d0dee534cfa45de87a4
Ameya Shenoy <shenoy.ameya@gmail.com>
2018-05-04 13:16:52 +0530

    pip 10 fix

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-try: # for pip >= 10
-	from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-	from pip.req import parse_requirements
-import re, ast
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in bench_manager/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
@@ -24,6 +22,5 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
 )
