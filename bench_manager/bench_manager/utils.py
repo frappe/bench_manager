f1ad3520c84f13fd1d0dfebb4e7922fd4de8746d
Ameya Shenoy <shenoy.ameya@gmail.com>
2018-07-09 10:01:31 +0000

    Python 3 fixes for checkoutput using safe decode

--- bench_manager/bench_manager/utils.py
+++ bench_manager/bench_manager/utils.py
@@ -5,36 +5,35 @@
 
 import frappe
 from frappe.model.document import Document
-from subprocess import Popen, check_output, PIPE, STDOUT
+from subprocess import Popen, PIPE, STDOUT
 import re, shlex
-
 
 def run_command(commands, doctype, key, cwd='..', docname=' ', after_command=None):
 	verify_whitelisted_call()
 	start_time = frappe.utils.time.time()
-	console_dump = ''
-	logged_command = ' && '.join(commands)
-	logged_command += ' ' #to make sure passwords at the end of the commands are also hidden
+	console_dump = ""
+	logged_command = " && ".join(commands)
+	logged_command += " " #to make sure passwords at the end of the commands are also hidden
 	sensitive_data = ["--mariadb-root-password", "--admin-password", "--root-password"]
 	for password in sensitive_data:
 		logged_command = re.sub("{password} .*? ".format(password=password), '', logged_command, flags=re.DOTALL)
 	doc = frappe.get_doc({'doctype': 'Bench Manager Command', 'key': key, 'source': doctype+': '+docname,
-		 'command': logged_command, 'console': console_dump, 'status': 'Ongoing'})
+		'command': logged_command, 'console': console_dump, 'status': 'Ongoing'})
 	doc.insert()
 	frappe.db.commit()
 	frappe.publish_realtime(key, "Executing Command:\n{logged_command}\n\n".format(logged_command=logged_command), user=frappe.session.user)
 	try:
 		for command in commands:
 			terminal = Popen(shlex.split(command), stdin=PIPE, stdout=PIPE, stderr=STDOUT, cwd=cwd)
-			for c in iter(lambda: terminal.stdout.read(1), ''):
+			for c in iter(lambda: safe_decode(terminal.stdout.read(1)), ''):
 				frappe.publish_realtime(key, c, user=frappe.session.user)
 				console_dump += c
 		if terminal.wait():
 			_close_the_doc(start_time, key, console_dump, status='Failed', user=frappe.session.user)
 		else:
 			_close_the_doc(start_time, key, console_dump, status='Success', user=frappe.session.user)
-	except:
-		_close_the_doc(start_time, key, console_dump, status='Failed', user=frappe.session.user)
+	except Exception as e:
+		_close_the_doc(start_time, key, "{} \n\n{}".format(e, console_dump), status='Failed', user=frappe.session.user)
 	finally:
 		frappe.db.commit()
 		# hack: frappe.db.commit() to make sure the log created is robust,
